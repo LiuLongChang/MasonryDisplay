15867dbf64481e50cf154ff4a8f8e1e3ad4c70b8
LiuLongChang <1040719208@qq.com>
2015-12-03 18:02:01 +0800

    Masonry Display
    Practice Masonry Auto Layout

--- MasonryDisplay/ViewController.m
+++ MasonryDisplay/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "Masonry.h"
 
 @interface ViewController ()
 
@@ -17,6 +18,60 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    UIView * greenView = [UIView new];
+    greenView.backgroundColor = UIColor.greenColor;
+    greenView.layer.borderColor = UIColor.blackColor.CGColor;
+    greenView.layer.borderWidth = 2;
+    [self.view addSubview:greenView];
+    
+    
+    UIView * redView = [UIView new];
+    redView.backgroundColor = UIColor.redColor;
+    redView.layer.borderColor = UIColor.blackColor.CGColor;
+    redView.layer.borderWidth = 2;
+    [self.view addSubview:redView];
+    
+    UIView * blueView = [UIView new];
+    blueView.backgroundColor = UIColor.blueColor;
+    blueView.layer.borderColor = UIColor.blackColor.CGColor;
+    blueView.layer.borderWidth = 2;
+    [self.view addSubview:blueView];
+    
+    
+    
+    
+    CGFloat padding = 10;
+    [greenView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.mas_equalTo(padding);
+        make.left.mas_equalTo(padding);
+        make.right.mas_equalTo(redView.mas_left).offset(-padding);
+        make.bottom.mas_equalTo(blueView.mas_top).offset(-padding);
+        // 三个控件等高
+        make.height.mas_equalTo(@[redView, blueView]);
+        // 红、绿这两个控件等高
+        make.width.mas_equalTo(redView);
+    }];
+    
+    
+    [redView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.height.bottom.mas_equalTo(greenView);
+        make.right.mas_equalTo(-padding);
+        make.left.mas_equalTo(greenView.mas_right).offset(padding);
+    }];
+    
+    
+    
+    [blueView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.height.mas_equalTo(greenView);
+        make.bottom.mas_equalTo(-padding);
+        make.left.mas_equalTo(padding);
+        make.right.mas_equalTo(-padding);
+    }];
+    
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
