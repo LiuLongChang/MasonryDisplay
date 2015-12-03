937c8885c5855ef7d968db96828b3a37d8abfca2
LiuLongChang <1040719208@qq.com>
2015-12-03 16:44:50 +0800

    Initial Commit

---
+++ MasonryDisplay/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MasonryDisplay
+//
+//  Created by langyue on 15/12/3.
+//  Copyright © 2015年 langyue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MasonryDisplay/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MasonryDisplay
+//
+//  Created by langyue on 15/12/3.
+//  Copyright © 2015年 langyue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
